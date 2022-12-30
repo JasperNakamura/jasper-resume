77a795273fb7f55b2c69938079d953845a4e3124
JasperNakamura <jasperhibou@yahoo.com>
2022-12-29 17:52:12 -0700

    fix attempt

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages",
+  assetPrefix: isProduction ? '/{jasper-resume}' : '',
 }
 
 module.exports = nextConfig
