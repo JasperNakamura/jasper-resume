eed261e3522db0a30832cdd69956781b5d2e964f
JasperNakamura <jasperhibou@yahoo.com>
2023-02-03 14:47:01 -0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,121 +1,59 @@
-import IconColumn from './components/social.jsx'
-import Header from './components/header.jsx'
-import { Box, Container } from '@mui/system'
-import { Card, CardMedia } from '@mui/material'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
+import React from 'react';
+/* import 'bootstrap/dist/css/bootstrap.min.css';
+ */import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-  },
-];
-
-export default function Home() {
+const Index = () => {
   return (
-    <>
-      <Header/>
-      <div>
-        <IconColumn/>
-        {/* Main Content Area */}
-        <div style={{float: 'right', width: '95%', padding: '5em'}}>
-          <Box>
-            <h1>Jasper Van</h1>
-          </Box>
-          <Card sx={{maxWidth: '350px', borderRadius: '2em', float: 'right', marginRight: '10vw'}}>
-            <CardMedia
-              component="img"
-              height='500px'
-              alt="portrait"
-              image={"portrait.png"}/>
-          </Card>
-          <Box>
-            <p style={{width: '30%', paddingTop: '2em', paddingBottom: '2em'}}>
-              Hello! My name is Jasper Van and I am a programmer and front-end web developer. 
-              I am highly motivated and detail-oriented, with a strong foundation in HTML, CSS, and JavaScript. 
-              I have experience using modern frameworks and libraries such as React, and I am always looking to learn and improve my skills.
-              I am passionate about creating high-quality, user-friendly websites and applications, and I enjoy staying up to date with the latest trends and best practices in web development. 
-              I am a team player and thrive in collaborative environments, and I am excited to bring my skills and enthusiasm to new challenges and opportunities. 
-            </p>
-          </Box>
-
-          <ImageList sx={{ width: '60%', height: '50vh' }}>
-            {itemData.map((item) => (
-              <ImageListItem key={item.img}>
-                <img
-                  src={`${item.img}?w=248&fit=crop&auto=format`}
-                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  alt={item.title}
-                  loading="lazy"
-                />
-                <ImageListItemBar
-                  title={item.title}
-                  subtitle={<span>by: {item.author}</span>}
-                  position="below"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </div>
+    <div className="wrapper">
+      <div className="main-content">
+        <h1>Welcome to my Index Page!</h1>
       </div>
-    </>
+
+      <footer id="footer" className="text-center w-100">
+        <div className="container-fluid bg-orange p-5">
+          <h2>Webpage News</h2>
+          <div className="row">
+            <div className="col-12">
+            <p>Our league is always looking for sponsors, please contact us on how to get involved.</p>
+            <p>Questions or comments regarding the website or to post a clinic or tournament, please contact us.</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <p>Contact and Social Media</p>
+            <div className="d-flex justify-content-center">
+              {/* Email */}
+              <a href="mailto:schwartz@edmontonbasketball.org" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-envelope" viewBox="0 0 16 16">
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                </svg>
+              </a>
+              {/* Twitter */}
+              <a href="https://twitter.com/EdmBasketball" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-twitter" viewBox="0 0 16 16">
+                  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                </svg>
+              </a>
+              {/* Facebook */}
+              <a href="https://www.facebook.com/home.php?#/group.php?gid=33495227998" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-facebook" viewBox="0 0 16 16">
+                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                </svg>
+              </a>
+              {/* Youtube */}
+              <a href="https://www.youtube.com/channel/UCAc9gUjm-Kiq_irf-efGLfw" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-youtube" viewBox="0 0 16 16">
+                  <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        </div>
+      </footer>
+    </div>
   )
 }
+
+export default Index
